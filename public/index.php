f3ba716da3c9984bdafcf6a7bbe93c503c93f625
erdurano <erdurano@gmail.com>
2024-10-10 22:49:17 +0300

    url routing Changes to be committed: 	modified:   public/index.php
    Untracked files:
    	.vscode/

--- public/index.php
+++ public/index.php
@@ -1,27 +1,18 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-
-require __DIR__ . '/../src/models.php';
+use App\Controllers\GroupController;
+use App\Controllers\MessageController;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
-$model = new MyModel();
 
-$model_text = $model->get_text();
+$app->post('/groups', [GroupController::class, 'createGroup']);
+$app->get('/groups', [GroupController::class, 'listGroups']);
+$app->post('/groups/{group_id}/join', [GroupController::class, 'joinGroup']);
 
-$app->get(pattern: "/", callable: function (Request $request, Response $response, $args) {
-    $response->getBody()->write(string: "Hello World!");
-    return $response;
-});
-
-$app->get("/home/", function (Request $request, Response $response, $args) {
-    $response->getBody()->write(string: $GLOBALS["model"]->get_text());
-    return $response;
-});
-
+$app->post('/groups/{group_id}/messages', [MessageController::class, 'sendMessage']);
+$app->get('/groups/{group_id}/messages', [MessageController::class, 'listMessages']);
 
 $app->run();
